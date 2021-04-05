c5c7cc6d29bc7cfe95d8f65d4c495d8593419983
amincoding <aminabdedaiem@gmail.com>
2021-04-05 13:01:07 +0100

    amin

---
+++ CMakeFiles/AntSimulator.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/amin/Downloads/AntSimulator-master (1)/AntSimulator-master/src/config.cpp" "/home/amin/Downloads/AntSimulator-master (1)/AntSimulator-master/antsimilator/CMakeFiles/AntSimulator.dir/src/config.cpp.o"
+  "/home/amin/Downloads/AntSimulator-master (1)/AntSimulator-master/src/display_manager.cpp" "/home/amin/Downloads/AntSimulator-master (1)/AntSimulator-master/antsimilator/CMakeFiles/AntSimulator.dir/src/display_manager.cpp.o"
+  "/home/amin/Downloads/AntSimulator-master (1)/AntSimulator-master/src/main.cpp" "/home/amin/Downloads/AntSimulator-master (1)/AntSimulator-master/antsimilator/CMakeFiles/AntSimulator.dir/src/main.cpp.o"
+  "/home/amin/Downloads/AntSimulator-master (1)/AntSimulator-master/src/utils.cpp" "/home/amin/Downloads/AntSimulator-master (1)/AntSimulator-master/antsimilator/CMakeFiles/AntSimulator.dir/src/utils.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../lib"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/AntSimulator.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "AntSimulator"
+  "AntSimulator.pdb"
+  "CMakeFiles/AntSimulator.dir/src/config.cpp.o"
+  "CMakeFiles/AntSimulator.dir/src/display_manager.cpp.o"
+  "CMakeFiles/AntSimulator.dir/src/main.cpp.o"
+  "CMakeFiles/AntSimulator.dir/src/utils.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AntSimulator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
